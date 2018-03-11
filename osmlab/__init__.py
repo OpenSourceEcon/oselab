059bf6ebdba8c1882a1968af6a92c14db660ba11
Andy Mockler <asmockler@gmail.com>
2018-03-10 19:32:10 -0600

    Remove unused imports; clean up flask structure

--- osmlab/__init__.py
+++ osmlab/__init__.py
@@ -1,1 +1,5 @@
-from .osmlab import app
+from flask import Flask
+
+app = Flask(__name__)
+
+import osmlab.views
--- osmlab/views.py
+++ osmlab/views.py
@@ -1,15 +1,5 @@
-# imports
-import os
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
-
-
-app = Flask(__name__)  # create the application instance
-app.config.from_object(__name__)  # load config from this file , OGvis.py
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-))
+from flask import render_template
+from osmlab import app
 
 
 @app.route('/')
