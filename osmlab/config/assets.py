0b710f133555881f6d70a06e750ae1097dcf9543
Andy Mockler <asmockler@gmail.com>
2018-06-23 15:45:06 -0500

    Remove CDNs (resolves #25)

--- osmlab/config/assets.py
+++ osmlab/config/assets.py
@@ -3,11 +3,20 @@ from flask_assets import Environment, Bundle
 
 assets = Environment(app)
 
-# JavaScript asset bundle
+# Main Vendor JavaScript
+vendor_javascripts = Bundle(
+  'vendor/javascript/jquery-3.2.1.slim.min.js',
+  'vendor/javascript/popper.min.js',
+  'vendor/javascript/bootstrap.min.js',
+  output='build/main-%(version)s.js'
+)
+assets.register('vendor_javascripts', vendor_javascripts)
+
+# Main JavaScript asset bundle
 javascripts = Bundle('javascript/home/slideshow.js', filters='rjsmin', output='build/main-%(version)s.js')
 assets.register('javascripts', javascripts)
 
-# CSS asset bundle
+# Main CSS asset bundle
 stylesheets = Bundle(
   'css/global.css',
   'css/layout.css',
