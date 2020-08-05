77afde4a65c5eb8450cdc03e95b5bee2c4db61da
Richard Evans <rickecon@gmail.com>
2020-08-05 02:22:54 -0600

    Added Real GDP NPP plot and Git Tutorial and Tutorials section

--- oselab/views.py
+++ oselab/views.py
@@ -63,14 +63,21 @@ def gallery():
     """
     return render_template('gallery/index.html', title=build_page_title('Gallery'))
 
+@app.route('/gallery/usgdp_npp')
+def usgdp_npp():
+    """
+    Serve up the U.S. real GDP normalized peak plot viz
+    """
+    return render_template('gallery/usgdp_npp.html',
+                           title=build_page_title('U.S. Real GDP Normalized Peak Plot'))
 
 @app.route('/gallery/usempl_npp')
 def usempl_npp():
     """
     Serve up the US total nonfarm employment normalized peak plot viz
     """
-    return render_template('gallery/usempl_npp.html', title=build_page_title('U.S. Total Nonfarm Employment Normalized Peak Plot'))
-
+    return render_template('gallery/usempl_npp.html',
+                           title=build_page_title('U.S. Total Nonfarm Employment Normalized Peak Plot'))
 
 @app.route('/gallery/djia_npp_mth')
 def djia_npp_mth():
@@ -80,14 +87,12 @@ def djia_npp_mth():
     return render_template('gallery/djia_npp_mth.html',
                            title=build_page_title('DJIA Normalized Peak Plot'))
 
-
 @app.route('/gallery/marginal_effective_corporate_taxes')
 def marginal_effective_corporate_taxes():
     """
     Serve up the Marginal Effective Tax Rates on Corporate Investments viz
     """
     return render_template('gallery/marginal_effective_corporate_taxes.html', title=build_page_title('Marginal Effective Tax Rates on Corporate Investments'))
-
 
 @app.route('/gallery/overlapping_generations')
 def overlapping_generations():
@@ -96,13 +101,20 @@ def overlapping_generations():
     """
     return render_template('gallery/overlapping_generations.html', title=build_page_title('Overlapping Generations'))
 
-
 @app.route('/gallery/tax_increase_decrease')
 def increase_decrease():
     """
     Serve up the tax_increase_decrease viz
     """
     return render_template('gallery/tax_increase_decrease.html')
+
+@app.route('/tutorials')
+def tutorials():
+    """
+    Serve up the tutorials page
+    """
+    return render_template('tutorials/index.html',
+                           title=build_page_title('Tutorials'))
 
 @app.route('/blog/<post>')
 def blog_post(post):
