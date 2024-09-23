29d52b0e4a416a187926b50dce70fb78dada8af4
Richard Evans <rickecon@gmail.com>
2024-09-23 08:37:11 -0600

    Updated setup.py and LICENSE

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    longdesc = fh.read()
+
+setuptools.setup(
     name='oselab',
+    version='0.1.0',
+    author='Richard W. Evans',
+    license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    description="Websit for the Open Source Economics Laboratory",
+    long_description_content_type="text/markdown",
+    long_description=longdesc,
     packages=['oselab'],
     include_package_data=True,
+    include_packages=True,
+    python_requires='>=3.12, <3.13',
     install_requires=[
-        'flask',
+        'flask>=1.1.2',
         'Flask-Assets',
         'Flask-Markdown',
+        'Pygments>=2.2.0',
+        'gunicorn'
     ],
 )
